3097b4275349690a9e829c4b4e8763b605168ed0
Zarr D. Soul <raynezzar21@gmail.com>
2023-03-20 20:59:25 +0700

    Delete dist directory

--- dist/khemia.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const libraries_1 = require("./libraries");
-new libraries_1.IClient().login(process.env.TOKEN);
